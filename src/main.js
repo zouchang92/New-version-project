0b0b942beb76d3142acdbc0c36f6ab9805f4bb5d
zouchang92 <www.940405365@qq.com>
2019-10-31 16:04:41 +0800

    代码提交

--- src/main.js
+++ src/main.js
@@ -8,12 +8,10 @@ import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 import Avue from '@smallwei/avue'
 import '@smallwei/avue/lib/index.css'
 
-
-
 import '@/styles/index.scss' // global css
 
 // 添加自定义组件
-import Calendar from './components/Calendar/Calendar.vue'
+import Calendar from './components/Calendar/UCalendar.vue'
 
 import App from './App'
 import store from './store'
