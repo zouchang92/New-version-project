4772652231ead3170d8a74a5c7c67fe2f061158e
zouchang92 <940405365@qq.com>
2019-11-05 15:56:01 +0800

    代码提交

---
+++ src/components/Timetable/Timetable.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="timetable-content">
+    <ul v-for="(n,i) in 10" :key="i">
+      <li>
+        <p>08:00-08:40</p>
+      </li>
+      <li>
+        <p>规划经济学</p>
+      </li>
+      <li>
+        <p>王良军</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.timetable-content {
+  overflow: hidden;
+  ul {
+    list-style: none;
+    display: flex;
+    margin: 0px;
+    padding: 0px;
+    justify-content: space-around;
+    background: #f5f6fa;
+    margin-top: 10px;
+    li {
+      padding-left: 20px;
+      padding: 0px;
+      p {
+        font-size: 16px;
+        font-family: Source Han Sans CN;
+        font-weight: 400;
+        color: #000;
+      }
+    }
+  }
+}
+</style>
--- src/views/dashboard/Teacher/components/LineChart.vue
+++ src/views/dashboard/Teacher/components/LineChart.vue
@@ -3,133 +3,153 @@
 </template>
 
 <script>
-import echarts from 'echarts'
-require('echarts/theme/macarons') // echarts theme
-import resize from './mixins/resize'
+import echarts from "echarts";
+require("echarts/theme/macarons"); // echarts theme
+import resize from "./mixins/resize";
+
+const animationDuration = 3000;
 
 export default {
   mixins: [resize],
   props: {
     className: {
       type: String,
-      default: 'chart'
+      default: "chart"
     },
     width: {
       type: String,
-      default: '100%'
+      default: "100%"
     },
     height: {
       type: String,
-      default: '280px'
-    },
-    autoResize: {
-      type: Boolean,
-      default: true
-    },
-    chartData: {
-      type: Object,
-      required: true
+      default: "270px"
     }
   },
   data() {
     return {
-      chart: null
-    }
-  },
-  watch: {
-    chartData: {
-      deep: true,
-      handler(val) {
-        this.setOptions(val)
-      }
-    }
+      chart: null,
+      data: [
+        "一单元",
+        "二单元",
+        "月考",
+        "三单元",
+        "四单元",
+        "其中",
+        "一单元",
+        "五单元",
+        "六单元",
+        "月考",
+        "期末"
+      ],
+      list: ["2019年下学期成绩分析图"]
+    };
   },
   mounted() {
     this.$nextTick(() => {
-      this.initChart()
-    })
+      this.initChart();
+    });
   },
   beforeDestroy() {
     if (!this.chart) {
-      return
+      return;
     }
-    this.chart.dispose()
-    this.chart = null
+    this.chart.dispose();
+    this.chart = null;
   },
   methods: {
     initChart() {
-      this.chart = echarts.init(this.$el, 'macarons')
-      this.setOptions(this.chartData)
-    },
-    setOptions({ expectedData, actualData } = {}) {
+      this.chart = echarts.init(this.$el, "macarons");
       this.chart.setOption({
-        xAxis: {
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-          boundaryGap: false,
-          axisTick: {
-            show: false
+        title: {
+          text: this.list,
+          right: "4%",
+          textStyle: {
+            color: "#000",
+            fontSize: 13,
+            fontWeight: 600
           }
-        },
-        grid: {
-          left: 10,
-          right: 10,
-          bottom: 20,
-          top: 30,
-          containLabel: true
         },
         tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross'
-          },
-          padding: [5, 10]
-        },
-        yAxis: {
-          axisTick: {
-            show: false
-          }
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c}"
         },
         legend: {
-          data: ['班级平均分', '语文均分']
+          left: "4%",
+          data: [{ name: "班级平均分" }, { name: "语文平均分" }]
         },
-        series: [{
-          name: 'expected', itemStyle: {
-            normal: {
-              color: '#FF005A',
-              lineStyle: {
-                color: '#FF005A',
-                width: 2
+        xAxis: {
+          type: "category",
+          axisLine: {
+            lineStyle: {
+              color: "#272729" //x轴的颜色
+            }
+          },
+          splitLine: { show: true },
+          data: [
+            "一单元",
+            "二单元",
+            "月考",
+            "三单元",
+            "四单元",
+            "其中",
+            "一单元",
+            "五单元",
+            "六单元",
+            "月考",
+            "期末"
+          ]
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true
+        },
+        yAxis: {
+          type: "value",
+          axisLine: {
+            lineStyle: {
+              color: "#272729" //x轴的颜色
+            }
+          }
+        },
+        series: [
+          {
+            name: "班级平均分",
+            type: "line",
+            smooth: false,
+            symbol: "circle", //折线点设置为实心点
+            symbolSize: 4, //折线点的大小
+            data: [90, 86, 89, 87, 90, 79, 99, 95, 87, 99, 89],
+            lineStyle: {
+              normal: {
+                color: "#3AA1FF",
+                lineStyle: {
+                  color: "#3AA1FF" //折线的颜色
+                }
               }
             }
           },
-          smooth: true,
-          type: 'line',
-          data: expectedData,
-          animationDuration: 2800,
-          animationEasing: 'cubicInOut'
-        },
-        {
-          name: 'actual',
-          smooth: true,
-          type: 'line',
-          itemStyle: {
-            normal: {
-              color: '#3888fa',
-              lineStyle: {
-                color: '#3888fa',
-                width: 2
-              },
-              areaStyle: {
-                color: '#f3f8ff'
+          {
+            name: "语文平均分",
+            type: "line",
+            smooth: false,
+            symbol: "circle", //折线点设置为实心点
+            symbolSize: 4, //折线点的大小
+            data: [80, 97, 92, 88, 90, 89, 100, 75, 80, 79, 80],
+            lineStyle: {
+              normal: {
+                color: "#FBD437",
+                lineStyle: {
+                  color: "#FBD437" //折线的颜色
+                }
               }
             }
-          },
-          data: actualData,
-          animationDuration: 2800,
-          animationEasing: 'quadraticOut'
-        }]
-      })
+          }
+        ]
+      });
     }
   }
-}
+};
 </script>
+
